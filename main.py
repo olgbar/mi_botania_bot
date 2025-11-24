5899b1e8dec66fe67181f52c740df324f22b4c8c
olgbar <barbaraolguin02@gmail.com>
2025-11-24 10:47:25 -0300

    agrego endpoint

--- main.py
+++ main.py
@@ -292,3 +292,8 @@ if __name__ == "__main__":
 
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
+
+@app.route("/", methods=["GET"])
+def home():
+    return "Botania Bot está funcionando 🌱"
