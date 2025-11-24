c7fb95d2ca44606bf570732fa8c5d1764265c52f
olgbar <barbaraolguin02@gmail.com>
2025-11-24 10:38:39 -0300

    modifico variables

--- config.py
+++ config.py
@@ -3,4 +3,13 @@ import os
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
-DATABASE_PATH = os.getenv("DATABASE_PATH", "botania.db")
+# Si estoy en Render → uso /opt/render/project/src/data
+if os.getenv("RENDER"):
+    DB_DIR = "/opt/render/project/src/data"
+else:
+    DB_DIR = "data"   # Carpeta local en computadora
+
+# Crear carpeta si no existe
+os.makedirs(DB_DIR, exist_ok=True)
+
+DATABASE_PATH = os.path.join(DB_DIR, "botania.db")
